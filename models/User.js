3f1fd1b8d6b51902a7c319b5230cf48cb58df0a1
aakib7 <ajmehdi5@gmail.com>
2022-07-08 20:47:26 +0500

    create update User shema

--- models/User.js
+++ models/User.js
@@ -19,6 +19,18 @@ const userSchema = mongoose.Schema({
         minLength:[6,"Password must be at least 6 characters"],
         select: false, // not select on select query
     },
+    gigs:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Gig",
+        }
+    ],
+    jobs:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Job",
+        }
+    ],
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 });
